94fd5cd4f0331d67938f1d0d4b4fe2624a3b3cee
vitorbech <vitorhbech@gmail.com>
2026-05-14 14:16:29 -0300

    feat: create Ticket model and database migration

--- alembic/env.py
+++ alembic/env.py
@@ -16,6 +16,7 @@ if config.config_file_name is not None:
 
 from app.models.database import Base
 from app.models.user import User
+from app.models.ticket import Ticket
 
 target_metadata = Base.metadata
 
